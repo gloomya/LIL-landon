3a2d0994c10154dbe8871cfec0678a9a6e2a14e1
avolodkovich <avolodkovich@katalystadvantage.com>
2022-07-08 12:58:52 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React in one day
         </a>
       </header>
     </div>
